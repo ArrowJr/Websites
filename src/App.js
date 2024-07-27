788023df20cb86a4e66d1370fdddc6c18abfc6db
ArrowJr <nisvar223@gmail.com>
2024-07-27 16:32:55 +0530

    Creating a new 3d object in here
    Used meshDepthMaterial for experimentation

--- src/App.js
+++ src/App.js
@@ -1,2 +1,24 @@
 import { useState, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
+
+function Cube(props)
+{
+  const wireframe  = useRef();
+  return (
+      <mesh {...props} ref={wireframe}>
+        <boxGeometry args={[2,2,2]}></boxGeometry>
+        <meshDepthMaterial blendColor={"blue"}></meshDepthMaterial>
+      </mesh>  
+  )
+}
+
+
+export default App() {
+  return (
+    <Canvas>
+      <ambientLight castShadow="true"></ambientLight>
+      <Box position={[0,0,0]}></Box>
+    </Canvas>
+  )
+
+}
